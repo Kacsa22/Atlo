897e22877a63e506469fb5215baf6778ae795b77
Kacsa22 <kovacs.patrik@hotmail.com>
2019-02-16 21:06:10 +0100

    Tovább fejlesztett kód

--- app/src/main/java/com/example/patrik/atlo/MainActivity.java
+++ app/src/main/java/com/example/patrik/atlo/MainActivity.java
@@ -10,11 +10,120 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText oldalAEdit;
+    EditText oldalBEdit;
+    EditText oldalCEdit;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initGUI();
+    }
 
+    void initGUI(){
+        oldalAEdit = findViewById(R.id.oldalAEdit);
+        oldalBEdit = findViewById(R.id.oldalBEdit);
+        oldalCEdit = findViewById(R.id.oldalCEdit);
+    }
+
+    public void Click(View V){
+        eredmeny Eredmeny = new eredmeny(oldalAEdit.getText().toString(), oldalBEdit.getText().toString(), oldalCEdit.getText().toString());
+    }
+
+    class eredmeny{
+
+        String aOldal;
+        String bOldal;
+        String cOldal;
+
+        public eredmeny(String aOldal, String bOldal, String cOldal) {
+            this.aOldal = aOldal;
+            this.bOldal = bOldal;
+            this.cOldal = cOldal;
+            ellenoriz();
+        }
+
+        void ellenoriz(){
+
+            Boolean felt1 = aOldal.equals("");
+            Boolean felt2 = bOldal.equals("");
+            Boolean felt3 = cOldal.equals("");
+
+            if (felt1 && !felt2 && !felt3){
+                befogoszamol(bOldal, cOldal, oldalAEdit);
+            }
+
+            else if (!felt1 && felt2 && !felt3){
+                befogoszamol(aOldal,cOldal, oldalBEdit);
+            }
+
+            else if (!felt1 && !felt2 && felt3){
+                atfogoszamol(aOldal, bOldal);
+
+            }
+            else{
+                Toast.makeText(MainActivity.this,"Egy mezőt üresen kell hagyni!!!",Toast.LENGTH_LONG).show();
+            }
+        }
+
+        void befogoszamol(String befogo, String atfogo, EditText edit){
+
+            Double dBefogo, dAtfogo, eredmeny;
+            eredmeny = 0.0;
+
+            try{
+                dBefogo = Double.parseDouble(befogo);
+                dAtfogo = Double.parseDouble(atfogo);
+
+                dBefogo = dBefogo * dBefogo;
+                dAtfogo = dAtfogo * dAtfogo;
+                eredmeny = Math.sqrt(dAtfogo - dBefogo);
+            }
+            catch (Exception e){
+                Toast.makeText(MainActivity.this, "Karakterhiba!!!",Toast.LENGTH_LONG).show();
+            }
+
+            eredmeny = kerekit(eredmeny,3);
+
+            if (!(eredmeny == 0)) {
+                edit.setText(eredmeny.toString());
+            }
+            else edit.setText("");
+
+        }
+
+        void atfogoszamol(String befogo1, String befogo2){
+
+            Double dBefogo1, dBefogo2, eredmeny;
+            eredmeny = 0.0;
+
+            try{
+                dBefogo1 = Double.parseDouble(befogo1);
+                dBefogo2 = Double.parseDouble(befogo2);
+
+                dBefogo1 = dBefogo1 * dBefogo1;
+                dBefogo2 = dBefogo2 * dBefogo2;
+                eredmeny = Math.sqrt(dBefogo1 + dBefogo2);
+            }
+            catch (Exception e){
+                Toast.makeText(MainActivity.this, "Karakterhiba!!!",Toast.LENGTH_LONG).show();
+            }
+
+            eredmeny = kerekit(eredmeny,3);
+
+            if (!(eredmeny == 0)) {
+                oldalCEdit.setText(eredmeny.toString());
+            }
+            else oldalCEdit.setText("");
+        }
+
+        double kerekit(double szam, int jegy){
+            szam = szam * Math.pow(10, jegy);
+            szam = Math.round(szam);
+            szam = szam / Math.pow(10, jegy);
+            return szam;
+        }
     }
 
 
